9603fc36b97de3a7a404610317a6521778c1aba2
pablo.echeverri <pablo.echeverri@sofka.com.co>
2022-07-13 11:29:44 -0500

    Resolve Kata

--- src/main/java/challenge/ReactiveExample.java
+++ src/main/java/challenge/ReactiveExample.java
@@ -4,7 +4,12 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
+import java.util.List;
+import java.util.Optional;
+import java.util.TreeSet;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 public class ReactiveExample {
@@ -15,19 +20,23 @@ public class ReactiveExample {
     public ReactiveExample() {
         //TODO: convertir los estudiantes a un Flux
 
-       /* estudianteList = List.of(
+        var list= List.of(
                 new Estudiante("raul", 30, List.of(1, 2, 1, 4, 5)),
                 new Estudiante("andres", 35, List.of(4, 2, 4, 3, 5)),
                 new Estudiante("juan", 75, List.of(3, 2, 4, 5, 5)),
                 new Estudiante("pedro", 80, List.of(5, 5, 4, 5, 5)),
                 new Estudiante("santiago", 40, List.of(4, 5, 4, 5, 5))
         );
-*/
+
+        estudianteList = Flux.fromIterable(list);
+
     }
 
     //TODO: suma de puntajes
     public Mono<Integer> sumaDePuntajes() {
-        return null;
+        return estudianteList
+                .map(Estudiante::getPuntaje)
+                .reduce(Integer::sum);
     }
 
     private Function<Estudiante, Integer> mapeoDeEstudianteAPuntaje() {
@@ -35,37 +44,64 @@ public class ReactiveExample {
     }
 
     //TODO: mayor puntaje de estudiante
-    public Mono<Estudiante> mayorPuntajeDeEstudiante(int limit) {
-        return null;
+    public Flux<Estudiante> mayorPuntajeDeEstudiante(int limit) {
+
+        return Flux.fromIterable(new TreeSet<>(estudianteList.toStream().collect(Collectors.toList()))
+                .descendingSet()
+                .stream()
+                .limit(limit).collect(Collectors.toList()));
 
     }
 
     //TODO: total de asisntencias de estudiantes con mayor puntaje basado en un  valor
     public Mono<Integer> totalDeAsisntenciasDeEstudiantesConMayorPuntajeDe(int valor) {
-        return null;
+        return estudianteList
+                .filter(estudiante -> estudiante.getPuntaje() >= valor)
+                .flatMap(estudiante -> Flux.fromIterable(estudiante.getAsistencias()))
+                .reduce(0, Integer::sum);
+    }
+
+    private Predicate<Estudiante> asistenciasPemitidas() {
+        return estudiante -> estudiante.getAsistencias()
+                .stream()
+                .reduce(0, Integer::sum) >= VALOR_PERMITIDO;
     }
 
     //TODO: el estudiante tiene asistencias correctas
     public Mono<Boolean> elEstudianteTieneAsistenciasCorrectas(Estudiante estudiante) {
-        return null;
+        return Mono.just(estudiante)
+                .filter(this.asistenciasPemitidas())
+                .map(estudiante1 -> true)
+                .switchIfEmpty(Mono.just(false));
     }
 
     //TODO: promedio de puntajes por estudiantes
     public Mono<Double> promedioDePuntajesPorEstudiantes() {
-        return null;
+        return estudianteList.collect(Collectors.averagingDouble(Estudiante::getPuntaje));
     }
 
 
     //TODO: los nombres de estudiante con puntaje mayor a un valor
     public Flux<String> losNombresDeEstudianteConPuntajeMayorA(int valor) {
-        return null;
+        return estudianteList.filter(estudiante -> estudiante.getPuntaje() > valor)
+                .map(Estudiante::getNombre);
     }
 
-
+    private Estudiante aprobar(Estudiante estudiante) {
+        return Optional.of(estudiante)
+                .filter(e -> e.getPuntaje() >= 75)
+                .map(e -> {
+                    var est1 = new Estudiante(e.getNombre(), e.getPuntaje(), e.getAsistencias());
+                    est1.setAprobado(true);
+                    return est1;
+                }).orElseGet(() -> estudiante);
+    }
 
     //TODO: estudiantes aprovados
     public Flux<String> estudiantesAprovados(){
-        return null;
+        return estudianteList.map(this::aprobar)
+                .filter(Estudiante::isAprobado)
+                .map(Estudiante::getNombre);
     }
 
 
--- src/test/java/challenge/ReactiveExampleTest.java
+++ src/test/java/challenge/ReactiveExampleTest.java
@@ -1,9 +1,14 @@
 package challenge;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -16,30 +21,57 @@ class ReactiveExampleTest {
     @Test
     void sumaDePuntajes(){
 
+        var valor = reactiveExample.sumaDePuntajes();
+
+        Mono<Integer> esperado = Mono.just(260);
+        //assert
+        Assertions.assertEquals(260, valor.block());
+
     }
 
     @Test
     void mayorPuntajeDeEstudiante(){
 
+        var valor = reactiveExample.mayorPuntajeDeEstudiante(1);
+        //assert
+
+        Assertions.assertEquals("pedro", valor.blockFirst().getNombre());
+
     }
 
     @Test
     void totalDeAsisntenciasDeEstudiantesComMayorPuntajeDe(){
-
+        //arrange
+        //act
+        var valor = reactiveExample.totalDeAsisntenciasDeEstudiantesConMayorPuntajeDe(75);
+        //assert
+        System.out.println(valor.block());
     }
 
     @Test
     void elEstudianteTieneAsistenciasCorrectas(){
-
+        var valor = reactiveExample.elEstudianteTieneAsistenciasCorrectas(
+                new Estudiante("raul", 30, List.of(5,2,1,4,5))
+        );
+        //assert
+        Assertions.assertTrue(valor.block());
     }
 
     @Test
     void promedioDePuntajesPorEstudiantes(){
-
+        //arrange
+        //act
+        var valor = reactiveExample.promedioDePuntajesPorEstudiantes();
+        //assert
+        Assertions.assertEquals(52, valor.block());
     }
 
     @Test
     void estudiantesAprovados(){
-
+        //arrange
+        //act
+        var valor = reactiveExample.estudiantesAprovados();
+        //assert
+        System.out.println(valor);
     }
 }
